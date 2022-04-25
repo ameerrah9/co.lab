5185e69c3ca928e377abd639e6189cd5afae73fc
Sara <amiri_gs@hotmail.com>
2022-04-25 18:55:58 -0400

    committing before merging

---
+++ prompts.js
@@ -0,0 +1,13 @@
+const bodyEl = document.querySelector('body');
+console.log('what up');
+setInterval(function() {
+    console.log('hello');
+    bodyEl.innerHTML = `
+    <div class="popup"
+        <h1>Don't Forget To Take Care of Yourself</h1>
+        <h3>Where would you like to take you next vacation?</h3>
+
+        <h1>Take a few moments for yourself & journal on your reflections</h1>
+    </div>
+    `
+}, 1000);
